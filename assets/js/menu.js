9ce25edc2829b5218a06fc722659dce5f68d7929
gopalakrishnan-r <rgopalakrishnanmca@gmail.com>
2021-09-24 16:04:35 +0530

    Data migration pages Updated

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,13 +1,18 @@
+/*
+    Menu json append to All HTML pages.
+    Developed on 23/09/2021
+*/
 $(document).ready(function() {
 
     // service drop dowm menu
-
     $.getJSON("/assets/data/service-menu.json",
         function(data) {
             var serviceMenu = ''
             $.each(data, function(key, value) {
                 serviceMenu += '<li>'
-                serviceMenu += '<a class="dropdown-item" href="' + value.url + '">' + value.menuName + '</a>'
+                serviceMenu += '<a class="dropdown-item" href="' + value.url + '">'
+                serviceMenu += value.menuName
+                serviceMenu += '</a>'
                 serviceMenu += '</li>'
             });
             $('#service-menu').append(serviceMenu);
